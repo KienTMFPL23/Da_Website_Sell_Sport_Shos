f08496150357d8b272be91bcb4801ff4ec55937e
KienTMFPL23 <117133352+KienTMFPL23@users.noreply.github.com>
2023-07-07 22:40:29 +0700

    update crud san pham

--- src/main/java/com/poly/controller/admin/SanPhamController.java
+++ src/main/java/com/poly/controller/admin/SanPhamController.java
@@ -2,6 +2,7 @@ package com.poly.controller.admin;
 
 import com.poly.entity.SanPham;
 import com.poly.service.SanPhamService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,45 +20,52 @@ public class SanPhamController {
     @GetMapping("/san-pham/hien-thi")
     public String hienthi(Model model) {
         model.addAttribute("listSP", sanPhamService.findALlSP());
-        return "san_pham/index";
+        model.addAttribute("view", "../san_pham/index.jsp");
+        return "admin/index";
     }
 
     @GetMapping("/san-pham/view-add")
     public String viewAdd(Model model, @ModelAttribute("SP") SanPham sanPham) {
         model.addAttribute("action", "/san-pham/add");
-        return "san_pham/add_update";
+        model.addAttribute("view", "../san_pham/add_update.jsp");
+        return "admin/index";
     }
 
     @GetMapping("/san-pham/view-update/{id}")
     public String viewUpdate(Model model, @PathVariable("id") UUID id, @ModelAttribute("SP") SanPham sanPham) {
+        model.addAttribute("view", "../san_pham/add_update.jsp");
         SanPham product = sanPhamService.getOne(id);
         model.addAttribute("action", "/san-pham/update/" + product.getId());
         SanPham sp = sanPhamService.getOne(id);
         model.addAttribute("SP", sp);
-        return "san_pham/add_update";
+        return "admin/index";
     }
 
     @PostMapping("/san-pham/add")
-    public String add(Model model, @ModelAttribute("SP") SanPham sanPham, BindingResult result) {
+    public String add(Model model, @Valid @ModelAttribute("SP") SanPham sanPham, BindingResult result) {
         Boolean hasError = result.hasErrors();
         SanPham product = sanPhamService.getSP(sanPham.getMaSP());
         if (product != null) {
             hasError = true;
             model.addAttribute("maspError", "Vui lòng không nhập trùng mã");
-            return "san_pham/add_update";
+            model.addAttribute("view", "../san_pham/add_update.jsp");
+            return "admin/index";
         }
         if (hasError) {
-            return "san_pham/add_update";
+            model.addAttribute("view", "../san_pham/add_update.jsp");
+            return "admin/index";
         }
         sanPhamService.addSP(sanPham);
         return "redirect:/san-pham/hien-thi";
     }
 
     @PostMapping("/san-pham/update/{id}")
-    public String update(@PathVariable("id") UUID id, Model model, @ModelAttribute("SP") SanPham sanPham,
+    public String update(@PathVariable("id") UUID id, Model model,@Valid @ModelAttribute("SP") SanPham sanPham,
                          BindingResult result) {
-        if (result.hasErrors()) {
-            return "san_pham/add_update";
+        Boolean hasError = result.hasErrors();
+        if (hasError) {
+            model.addAttribute("view", "../san_pham/add_update.jsp");
+            return "admin/index";
         }
         SanPham sp = sanPhamService.getOne(id);
         sp.setMaSP(sanPham.getMaSP());
